aab95733ed7d2baf083c73b4a0f3735b253427bc
jamesebune <james.ebune@gmail.com>
2024-04-10 22:15:16 +0100

    commit

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
 console.log("Hello World");
-console.log("Hello D");
-// https://github.com/jamesebune/Foodies_Haven.git
-// git@github.com:jamesebune/demo-repo.git
-// git@github.com:jamesebune/Foodies_Haven.git

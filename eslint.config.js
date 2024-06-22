d9cf85ddcc4e571dab53b6bde35c64e66f47e1a2
jamesebune <james.ebune@gmail.com>
2024-06-22 13:00:06 +0100

    jest setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
--- src/script.test.js
+++ src/script.test.js
@@ -1,11 +1,13 @@
-import { expect, test } from "@jest/globals";
-//const { describe, expect, test } = require("@jest/globals");
+// import { expect, test } from "@jest/globals";
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { expect, test } = require("@jest/globals");
 // import { expect, test } from "@jest/globals";
 // import test from "@jest/globals";
 // import expect from "@jest/globals";
-//import sum from "./script.ts";
- 
-import sum from "./script.ts";
+// import sum from "./script.ts";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const sum = require("./script.ts");
 
 test("add 1 & 2", () => {
   expect(sum(1, 2)).toBe(3);
